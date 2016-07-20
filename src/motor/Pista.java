e28e945b7db725c1659adad5baa525f6098294d2
Giovanni2293 <otaku2293@gmail.com>
2016-07-20 11:32:13 -0500

    Se añaden todas las clases pertinentes y una prueba con una libreria de musica

--- src/motor/Pista.java
+++ src/motor/Pista.java
@@ -1,5 +1,19 @@
 package motor;
 
+import jm.JMC;
+import jm.music.data.Note;
+import jm.util.Play;
+
 public class Pista {
+	
+	/*public static void main (String[] args)
+	{
+		//C2 a C7
+		Note note= new Note();
+		note.setPitch(JMC.c6);
+	   
+		Play.midi(note);
+		
+	}*/
 
 }
