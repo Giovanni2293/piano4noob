5766c56676de90cf93b4b742c004c8bf3845432e
taroufelinxg1305 <wichoprada13_05@hotmail.com>
2016-09-07 22:41:23 -0500

    cosas del modo aprendizaje y practica

--- src/motor/AprendizajePista.java
+++ src/motor/AprendizajePista.java
@@ -4,42 +4,33 @@ import jm.music.data.Note;
 import jm.music.data.Score;
 import utilidad.Aleatorios;
 
-public class AprendizajePista {
+public class AprendizajePista extends InterfacePista{
 
-	public static Aleatorios ran = new Aleatorios();
-	static AprendizajePista ap;
-	public static Tecla[] tecArray;
-	public Note[] noteArray;
+	
 
 	public AprendizajePista(int num) {
-		tecArray = new Tecla[num];
-		noteArray= new Note[num];
-		for (int i = 0; i < num; i++) {
-			tecArray[i]=ran.getRandomTecla();
-			noteArray[i]=tecArray[i].getSonido();
-		}
+		super(num);
 	}
 
 	public static void main(String[] args) {
 		ap = new AprendizajePista(50);
-		ap.evaluar("",tecArray[0]);
-		ap.evaluar(tecArray[1].getNombreNatural(),tecArray[1]);
-		ap.evaluar("",tecArray[2]);
-		ap.evaluar(tecArray[3].getNombreNatural(),tecArray[3]);
-		System.out.println(Puntuacion.getPuntuacion());
+		ap.evaluar("",getTecArray(0));
+		ap.evaluar(getTecArray(1).getNombreNatural(),getTecArray(1));
+		ap.evaluar("",getTecArray(2));
+		ap.evaluar(getTecArray(3).getNombreNatural(),getTecArray(3));
 
 	}
 
 	public void evaluar(String tocada, Tecla sugerida) {
-		
+
 		if (tocada.equals(sugerida.getNombreNatural())) {
-			Puntuacion.acierto();
 			System.out.println("bien");
 		} else {
-			Puntuacion.fallos();
 			System.out.println("mal");
 
 		}
+
 	}
+
 
 }
---
+++ src/motor/InterfacePista.java
@@ -0,0 +1,33 @@
+package motor;
+
+import java.util.ArrayList;
+
+import jm.music.data.Note;
+import jm.music.data.Score;
+import utilidad.Aleatorios;
+
+public abstract class InterfacePista {
+
+	public Aleatorios ran = new Aleatorios();
+	static AprendizajePista ap;
+	private static Tecla[] tecArray;
+	public static Note[] noteArray;
+
+	public static Tecla getTecArray(int i) {
+		return tecArray[i];
+	}
+
+	public Note getNoteArray(int i) {
+		return noteArray[i];
+	}
+
+	public InterfacePista(int num) {
+		tecArray = new Tecla[num];
+		noteArray = new Note[num];
+		for (int i = 0; i < num; i++) {
+			tecArray[i] = ran.getRandomTecla();
+			noteArray[i] = tecArray[i].getSonido();
+		}
+	}
+
+}
